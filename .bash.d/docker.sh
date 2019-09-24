5b5e6c4e09e9d565e68ce61ea5569a40d3da3784
Alden Quimby <aldenquimby@gmail.com>
2019-09-24 08:37:15 -0400

    Update bash helpers

--- .bash.d/docker.sh
+++ .bash.d/docker.sh
@@ -1,3 +1,12 @@
+# usage: docker_bash redis
 docker_bash() {
-	docker exec -it $1 bash
+  docker exec -it $1 bash
 }
+
+# usage: dv_start rmsfe
+dv_start() {
+  # docker-compose pull --include-deps $1 &&
+  export TAG=master && docker-compose up -d --no-deps --remove-orphans --force-recreate $1
+}
+
+# to STOP a container: docker-compose stop haproxy
