c2150420207492b9981fd4279928ce97aeed0488
Alden Quimby <aldenquimby@gmail.com>
2019-09-24 08:41:07 -0400

    Update setupMac.sh

--- setupMac.sh
+++ setupMac.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
-# install config files
+echo "Copying .vimrc"
 cp .vimrc ~/.vimrc
+
+echo "Copying .gitconfig"
 cp .gitconfig ~/.gitconfig
+
+echo "Copying .bash_profile"
 cp .bash_profile ~/.bash_profile
 
-# make nvm working directory
+echo "Making .nvm directory"
 mkdir ~/.nvm
 
-# install packages
+echo "Installing brew packages"
 sh ./brew.sh
 
-# setup git credential helper
-echo "Installing git https credential helper... "
-curl -s -O \
-  https://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
+echo "Installing git https credential helper"
+curl -s -O https://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
 chmod u+x git-credential-osxkeychain
-sudo mv git-credential-osxkeychain \
-  "$(dirname $(which git))/git-credential-osxkeychain"
+sudo mv git-credential-osxkeychain "$(dirname $(which git))/git-credential-osxkeychain"
 git config --global credential.helper osxkeychain
-
-# setup vundle
-if [ ! -d "${HOME}/.vim/bundle/vundle" ]; then
-  git clone https://github.com/gmarik/vundle.vim ~/.vim/bundle/vundle
-fi
-
-# create colors directory for vim
-if [ ! -d "${HOME}/.vim/colors" ]; then
-  mkdir ~/.vim/colors
-fi
-
-# install vim bundles
-vim +BundleInstall +qall
